5d5f6bc2b2b1e09a0d7b8208e9b539d16efca5df
Islam Almersawi <islam.almersawi@openinnovation.ai>
2025-02-10 12:00:24 +0400

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name="onprem-shai",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=["click", "openai", "inquirer"],
     entry_points={
